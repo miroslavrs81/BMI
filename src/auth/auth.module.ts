0d1e56f4eb46c815633effc4fede9179513e20cf
miroslavrs81 <miroslav81ilic@gmail.com>
2023-12-16 16:47:37 +0100

    Update Auth Module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,11 +3,19 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/entities/user.entity';
-import { JwtService } from '@nestjs/jwt';
+import { JwtModule, JwtService } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: process.env.JWT_EXPIRATION_TIME },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService, JwtService],
+  exports: [AuthService],
 })
 export class AuthModule {}
--- src/helpers/error-message-mapper.helper.ts
+++ src/helpers/error-message-mapper.helper.ts
@@ -2,4 +2,8 @@ export const returnMessages = Object.freeze({
   EmailInUse: 'Email is in use!',
   WorkoutNameLength: 'workoutName must be between 1 and 255 characters!',
   WorkoutNotFound: 'Workout not found!',
+  UserNotCreated: 'Something went wrong! User is not created!',
+  EmailPasswordValidation: 'Invalid password or email!',
+  EmailNotVerified: 'Email is not verified!',
+  UserSuccessfullyLoggedIn: 'You have been successfully logged in!',
 });
